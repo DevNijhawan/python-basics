c1245718f4841a0b0c797642bead6a9f35090017
Dev Nijhawan <devnijhawan2005@gmail.com>
2026-07-21 23:13:34 +0530

    Loops

--- .dist/.dist/Loops.py
+++ .dist/.dist/Loops.py
@@ -1,1 +1,5 @@
 # Each repetation is called Itreation
+
+# Defination of for loop
+# A for loop in Python is used to repeat a block of code for each item in a sequence 
+# (such as a list, string, tuple, dictionary, or range).
