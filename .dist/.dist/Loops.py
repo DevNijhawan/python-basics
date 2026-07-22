a5b920c1573ac6dd5fcd8c131e734fa2f5701446
Dev Nijhawan <devnijhawan2005@gmail.com>
2026-07-22 23:49:37 +0530

    Loops

--- .dist/.dist/Loops.py
+++ .dist/.dist/Loops.py
@@ -3,3 +3,17 @@
 # Defination of for loop
 # A for loop in Python is used to repeat a block of code for each item in a sequence 
 # (such as a list, string, tuple, dictionary, or range).
+
+
+fruits = "banana apple orange grapes"
+
+print()
+
+for f in fruits:
+    print(f)
+
+
+
+
+
+    
