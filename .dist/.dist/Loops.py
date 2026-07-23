904a18d2634975533fb7f414ecfbc45d94483495
Dev Nijhawan <devnijhawan2005@gmail.com>
2026-07-24 00:00:33 +0530

    Range

--- .dist/.dist/Loops.py
+++ .dist/.dist/Loops.py
@@ -4,13 +4,45 @@
 # A for loop in Python is used to repeat a block of code for each item in a sequence 
 # (such as a list, string, tuple, dictionary, or range).
 
-
+"""
 fruits = "banana apple orange grapes"
 
 print()
 
 for f in fruits:
     print(f)
+    
+
+# range() function = range() generates a sequence of numbers. It doesn’t store all the numbers at once; 
+# instead, it produces them one by one, which makes it memory efficient.
+# syntax : range(start, stop ,step):
+# for revese range function we use negative indexing in steping
+
+
+for i in range(3, 33, 3):
+    print(i)
+
+# next range function is 
+#range(start,stop): this is use for that code that has default step 1 it will not change
+
+for w in range(1,4):
+    print(w)
+print("Happy Birthday")
+
+#next range(stop)
+# in this start has default value 0 and step 1 is also default
+
+for p in range(10):
+    print(p)
+    """
+
+
+item = ["chips","kurkure","salt","sugar","bread"]
+
+for g in range(len(item)):
+    print(g)
+
+# next we 
 
 
 
